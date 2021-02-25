94b4df7ed651ff7afe9182f2d91ed36ee2048a7b
Pedro Schellerg <pedro.schellerg@infracommerce.com.br>
2021-02-24 22:39:47 -0300

    Implementa o JWT

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# Instalação
+# Teste front-end Gorila
+
+## Instalação
+```bash
+$ npm install
+```
+
+## API
+Você pode conferir os endpoints da aplicação neste [link](https://documenter.getpostman.com/view/14696473/TWDamvPN).
 
 ## Banco de Dados
 Rode o comando abaixo para inicializar um banco de dados MySQL no Docker:  
-`docker run --name gorila -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root mysql`
+```bash
+$ docker run --name gorila -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root mysql
+```
 
-Usando a linha de comando ou um cliente de MySQL, importe o arquivo banco.sql
-
-# Documentação
-
-https://www.npmjs.com/package/mysql2
+Usando a linha de comando ou um cliente de MySQL, importe o arquivo [db.sql](/sql/db.sql)
--- index.js
+++ index.js
@@ -1,44 +1,113 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const { response } = require("express");
 
 const SERVER_PORT = 7777;
+const JWT_SECRET = 'mamacoquerbanana';
 
 const app = express();
 app.use(bodyParser.json());
 
+const secureRouteMiddleware = (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization.replace("Bearer ", "");
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            res.status(403).json({
+                message: err.message
+            });
+            return;
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
 // Rotas
 const loadRoutes = (conn) => {
-    app.get("/investment", async (request, response) => {
+    app.post("/login", async (req, res) => {
+        const { username, password} = req.body;
         try {
-            const [rows] = await conn.query("SELECT * FROM investments");
-            response.json(rows);
+            const [rows] = await conn.query(`SELECT * FROM users WHERE username = '${username}'`);
+
+            if (rows.length === 0) {
+                res.status(404).json({
+                    message: "Usuário não encontrado"
+                });
+                return;
+            }
+
+            const [user] = rows;
+            if (password !== user.password) {
+                res.status(404).json({
+                    message: "Senha incorreta"
+                });
+                return;
+            }
+
+            jwt.sign({
+                id: user.id,
+                username: user.username
+            }, JWT_SECRET, { expiresIn: '12h' }, (err, token) => {
+                if (err) {
+                    res.status(401).json({
+                        message: "Acesso negado"
+                    });
+                    return;
+                }
+                res.json(token);
+            });
+
         } catch (error) {
-            response.status(500).json({
+            res.status(500).json({
                 message: error.message,
             });
         }
     });
 
-    app.post("/investment", async (request, response) => {
+    app.get("/investment", secureRouteMiddleware, async (req, res) => {
         try {
-            const {type, value, date} = request.body;
-            const [rows] = await conn.query(`INSERT INTO investments (type, value, date) VALUES ('${type}', '${value}', '${date}')`);
-            response.json(rows);
+            const { user } = req;
+            const [rows] = await conn.query(`SELECT * FROM investments WHERE user_id = '${user.id}'`);
+            res.json(rows);
         } catch (error) {
-            response.status(500).json({
+            res.status(500).json({
                 message: error.message,
             });
         }
     });
 
-    app.delete("/investment/:id", async (request, response) => {
+    app.post("/investment", secureRouteMiddleware, async (req, res) => {
         try {
-            const { id } = request.params;
+            const { user, body: { type, value, date }} = req;
+            const [rows] = await conn.query(`INSERT INTO investments (type, value, date, user_id) VALUES ('${type}', '${value}', '${date}', '${user.id}')`);
+            res.json(rows);
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+    });
+
+    app.delete("/investment/:id", secureRouteMiddleware, async (req, res) => {
+        try {
+            const { user, params: { id }} = req;
+            const [userInvestments] = await conn.query(`SELECT * FROM investments WHERE user_id = ${user.id} AND id = ${id}`);
+
+            if (userInvestments.length === 0) {
+                res.status(403).json({
+                    message: "Operação negada"
+                });
+                return;
+            }
+
             const [rows] = await conn.query(`DELETE FROM investments WHERE id = ${id}`);
-            response.json(rows);
+            res.json(rows);
         } catch (error) {
-            response.status(500).json({
+            res.status(500).json({
                 message: error.message,
             });
         }
